40da5ec67342218551f90ec99d77f70fc5bd973c
aamircodes <aamirp1@outlook.com>
2022-04-19 14:03:07 +0100

    Fix navigation

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,14 +9,8 @@ function Home() {
   navigate('/ChooseService')
   return (
     <div>
-      <TopNav
-        company={
-          <TopNav.Anchor href='https://example.com' target='new'>
-            <TopNav.IconTitle icon={<img src={require('../assets/images/govuk-logotype-crown.png')} alt='test' />}>GOV.UK</TopNav.IconTitle>
-          </TopNav.Anchor>
-        }
-      />
       <div className='govuk-header__container govuk-width-container'>
+        <Header />
         <h1>Strategy and Advice </h1>
         <p> Use this service to:</p>
         <ul>
